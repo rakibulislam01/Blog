03502aacc67e01b4c11e947a964458b0b2e08364
rakibulislam01 <mrakibul70@gmail.com>
2018-12-23 20:30:06 +0600

    update

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -5,9 +5,9 @@ from django.conf.urls import url
 # This is For check
 urlpatterns = [
     url('create/', views.post_create),
-    url(r'^(?P<id>\d+)/$', views.post_detail),
-    url('postlist/', views.post_list),
-    url('update/', views.post_update),
-    url('delete/', views.post_delete),
+    url(r'^(?P<id>\d+)/$', views.post_detail, name="detail"),
+    url('postlist/', views.post_list, name="list"),
+    url(r'^(?P<id>\d+)/edit/$', views.post_update, name="update"),
+    url(r'^(?P<id>\d+)/delete/$', views.post_delete),
     # path('jango', views.index, name='jango'),
 ]
