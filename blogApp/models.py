8adcc250007fa1127937a09d056c27fbb8f6e614
rakibulislam01 <rakibulprince2013@gmail.com>
2019-01-03 20:25:43 +0600

    delete

--- blogApp/models.py
+++ blogApp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.urls import reverse
+
 
 # Create your models here.
 
@@ -7,10 +7,7 @@ class Post(models.Model):
     title       = models.CharField(max_length=200)
     content     = models.TextField()
     updated     = models.DateTimeField(auto_now=True, auto_now_add=False)
-    timestamp   = models.DateTimeField(auto_now=False, auto_now_add=True)
+    tiemstamp   = models.DateTimeField(auto_now=False, auto_now_add=True)
 
     def __str__(self):
         return self.title
-
-    def get_absolute_url(self):
-        return reverse("detail", kwargs={"id": self.id})
